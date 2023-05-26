8f38953867a241fd751b400ee802215fa3a7a2f3
suresh304 <sureshallie@gmail.com>
2023-05-26 15:50:37 +0530

    push-test

--- src/SignIn.js
+++ src/SignIn.js
@@ -13,7 +13,8 @@ signInWithPopup(auth, provider)
   .then((result) => {
     // This gives you a Google Access Token. You can use it to access the Google API.
     const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
+console.log(credential,"+++++++")
+	  const token = credential.accessToken;
     // The signed-in user info.
     const user = result.user;
     // IdP data available using getAdditionalUserInfo(result)
